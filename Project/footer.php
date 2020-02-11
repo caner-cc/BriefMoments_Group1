0c183d00a6fa098a40e337a5c87a23fbd51893a0
Caner Camci <camci.caner@gmail.com>
2020-02-11 11:27:48 +0200

    headerandfooter

---
+++ Project/footer.php
@@ -0,0 +1,11 @@
+<!-- footer -->
+<footer class="page-footer font-small blue bg-light">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3"><?php echo "&copy" . date("Y"); ?> BriefMoments
+    </div>
+    <!-- Copyright -->
+  
+</footer>
+</body>
+</html>
